6c78e1ad292ba79fdd1e2bad8d2bfd73d2e4c563
Shuo Huang <huangshuo@outlook.com>
2015-10-09 19:57:41 +0800

    1.webpack with babel and react support

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import React from 'react';
+
+class App extends React.Component {
+	render() {
+		return <div>This is just a starter kit</div>
+	}
+}
+
+React.render(<App />, document.body);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var webpack = require('webpack');
+
+module.exports = {
+	entry: [
+		'webpack/hot/only-dev-server',
+		'./src/js/app.js'
+	],
+	output: [
+		path: __dirname + '/dist',
+		filename: 'app.js'
+	],
+	module: {
+		loaders: [
+			{
+				test: /\.js?$/,
+				loaders: ['react-hot', 'babel'],
+				exclude: /node_modules/
+			},
+			{
+				test: /\.js?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/ 
+			},
+			{
+				test: /\.css$/,
+				loader: 'style!css'
+			}
+		]
+	},
+	plugins: [
+		new webpack.NoErrorsPlugin()
+	]
+};
